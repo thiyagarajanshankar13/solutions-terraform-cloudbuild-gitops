30e058fe01bead7b49fb775b4c8dad025fe6ca8c
Thiyagarajan <thiyagarajan.shankar13@gmail.com>
2022-04-24 20:11:49 +0000

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="datahub-316304"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "datahub-316304-tfstate"
     prefix = "env/prod"
   }
 }
